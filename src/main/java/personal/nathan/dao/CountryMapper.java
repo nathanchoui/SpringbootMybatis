e1ebae0960fde12d787327a1dbcef15e4292a65a
za-zhangwei002 <wei.zhang@zatech.com>
2019-02-15 10:03:52 +0800

    update

--- src/main/java/personal/nathan/dao/CountryMapper.java
+++ src/main/java/personal/nathan/dao/CountryMapper.java
@@ -1,11 +1,16 @@
 package personal.nathan.dao;
 
+import jdk.management.resource.internal.inst.FileOutputStreamRMHooks;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import personal.nathan.model.dataObject.CountryDO;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Mapper
+
+
 public interface CountryMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -16,4 +21,5 @@ public interface CountryMapper {
     List<CountryDO> selectAll();
 
     int updateByPrimaryKey(CountryDO record);
+
 }
